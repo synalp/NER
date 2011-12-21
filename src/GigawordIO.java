3631813558871db82cb57f118cffe38282dccd4d
Christophe Cerisara <xtof@sivry.loria.fr>
2011-12-21 13:05:22 +0100

    giga ok

--- src/GigawordIO.java
+++ src/GigawordIO.java
@@ -7,6 +7,7 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -15,6 +16,7 @@ import java.util.zip.GZIPInputStream;
 import jsafran.DetGraph;
 import jsafran.GraphIO;
 import jsafran.JSafran;
+import jsafran.Mot;
 
 
 public class GigawordIO {
@@ -55,24 +57,88 @@ public class GigawordIO {
 		String p = chunkPaths.get(chunk);
 		try {
 			GZIPInputStream i = new GZIPInputStream(new FileInputStream(p));
-			BufferedReader f = new BufferedReader(new InputStreamReader(i));
-			
+			BufferedReader f = new BufferedReader(new InputStreamReader(i,Charset.forName("UTF-8")));
+			ArrayList<DetGraph> gs = new ArrayList<DetGraph>();
 			boolean istext = false;
-			for (;;) {
+			for (int z=0;;z++) {
 				String s = f.readLine();
 				if (s==null) break;
+				if (z%10000==0) System.out.println("lines "+z);
 				if (s.indexOf("<TEXT>")>=0) {
 					istext=true; continue;
 				}
+				if (s.indexOf("</TEXT>")>=0) {
+					istext=false; continue;
+				}
 				if (!istext) continue;
+				if (s.charAt(0)=='<') continue;
 				// tokenisation tres simple, car la segmentation plus complexe est bcp trop lente...
-				s=s.trim();
-				String[] st = s.split(" ");
-				for (int t=0;t<st.length;t++) {
-					if (st[t].length()>1) {
-						
+				s=s.replace(";", " ;");
+				s=s.replace(":", " :");
+				s=s.replace("!", " !");
+				s=s.replace("?", " ?");
+				s=s.replace("``", " `` ");
+				s=s.replace("''", " ''");
+				s=s.replace("/", " / ");
+				s=s.replace("\"", " \"");
+				s=s.replace("[", " [ ");
+				s=s.replace("]", " ] ");
+				s=s.replace("{", " { ");
+				s=s.replace("}", " } ");
+				s=s.replace("(", " ( ");
+				s=s.replace(")", " ) ");
+
+				// point
+				{
+					int x=0;
+					for (;;) {
+						x=s.indexOf('.',x);
+						if (x<0) break;
+						if (x>0) {
+							char c = s.charAt(x-1);
+							if (Character.isDigit(c)||Character.isUpperCase(c)) {
+								// on le laisse dans le mot
+							} else {
+								s=s.substring(0,x)+' '+s.substring(x);
+								++x;
+							}
+						}
+						++x;
 					}
 				}
+				
+				// virgule
+				{
+					int x=0,y=0;
+					for (;;) {
+						x=s.indexOf(',',y);
+						if (x<0) break;
+						if (x>0) {
+							char c = s.charAt(x-1);
+							if (Character.isDigit(c)) {
+								// on le laisse dans le mot
+							} else {
+								s=s.substring(0,x)+' '+s.substring(x);
+								++x;
+							}
+						}
+						y=x+1;
+					}
+				}
+				
+				s=s.replaceAll("  +", " ");
+				s=s.trim();
+				String[] st = s.split(" ");
+				DetGraph g = new DetGraph();
+				int idx=1;
+				for (int t=0;t<st.length;t++) {
+					if (st[t].length()>0) {
+						Mot m = new Mot(st[t].trim(), idx);
+						g.addMot(idx-1, m);
+						idx++;
+					}
+				}
+				gs.add(g);
 			}
 			f.close();
 			
