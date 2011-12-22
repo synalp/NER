1b24a4f6a8f4c5e6af3829cb409432acdbc7dfc6
Pavel Kral <kral@kral-laptop.(none)>
2011-12-22 12:14:43 +0100

    added lemmas

--- etape-cz.sh
+++ etape-cz.sh
@@ -9,10 +9,12 @@ else
 	setenv PATH $PATH":../../tools"
 endif
 
-rm res.log
-touch res.log
+set ens = (city country currency datetime e-subject geography nationality number organization problematic region religion sport sport-club)
 
-#goto train
+rm res.log res-all.log
+touch res.log res-all.log
+
+goto train
 
 echo "Corpus_preparation - transformation into the standford format"
 foreach en (figure)
@@ -29,41 +31,61 @@ awk -v ne=$en -f make-good-format.awk /global/agey/pavel/CTK/NER/$tt.txt >! ./$t
 
 awk 'BEGIN{i=1; }{if (NF >= 1) {print i"\t"$1; i++;} else {printf ("\n"); ; i=1;}}' $tt.tmp >! $tt.conll
 
+#lemmatization
 #java -Xmx2G -cp "$JCP" is2.lemmatizer.Lemmatizer -model ../syntponct/models/lemma-cz.model -test $tt.conll -out lemmas.$tt
 
-java -Xmx2G -cp  "$JCP" is2.tag3.Tagger -model ../syntponct/models/tag-cz.model -test $tt.conll -out $tt.POS
+#POS tagging
+#java -Xmx2G -cp  "$JCP" is2.tag3.Tagger -model ../syntponct/models/tag-cz.model -test $tt.conll -out $tt.POS
 
-paste ./$tt.tmp $tt.POS >! $tt.all
+paste ./$tt.tmp lemmas.$tt $tt.POS >! $tt.all
 
 #without error deletion
 #awk '{if (NF >=1) {if ($1 != "") {print $1 "\t" $8 "\t" $2;} } else {printf("\n");} }' $tt.all >! $tt.$en.tab
 
 #post-processing - errors deletion <a href ...
-awk '{if (NF >=1) {if ($1 != "") {print $1 "\t" $8 "\t" $2;} } else {printf("\n");} }' $tt.all | awk '{if ($3 == "" || $3 == "NO" || $3 == "figureB" || $3 == "figureI") print $0;}' >! $tt.$en.tab
+awk '{if (NF >=1) {if ($1 != "") {print $1 "\t" $6 "\t" $22 "\t" $2;} } else {printf("\n");} }' $tt.all | awk '{if ($4 == "" || $4 == "NO" || $4 == "figureB" || $4 == "figureI") print $0;}' >! $tt.$en.tab
+
+#exit
+end
+end
+
+foreach en ($ens)
+
+foreach tt (trn test)
+echo 'en='$en
+
+awk -v ne=$en -f make-good-format.awk /global/agey/pavel/CTK/NER/$tt.txt >! ./$tt.tmp
+paste ./$tt.tmp lemmas.$tt $tt.POS >! $tt.all
+
+#post-processing - errors deletion <a href ...
+awk -v en=$en '{if (NF >=1) {if ($1 != "") {print $1 "\t" $6 "\t" $22 "\t" $2;} } else {printf("\n");} }' $tt.all | awk '{if ($4 == "" || $4 == "NO" || index($4,en) > 0) print $0;}' >! $tt.$en.tab
 
 end
 end
-#exit
+
+exit
 
 train:
-echo "CRF training"
-foreach en (figure)
+foreach en (figure $ens)
+echo "CRF training of the "$en" entity..."
   sed 's,trainFile=synfeats0.tab,trainFile=trn.'$en'.tab,g' syncz.props >! tmp.props
    java -Xmx7g -cp ../jsafran/lib/detcrf.jar edu.stanford.nlp.ie.crf.CRFClassifier -prop tmp.props
   mv kiki.mods en.$en.mods
 #end
 
 test:
-echo "Testing"
+echo "Testing of the "$en" entity..."
  java -Xmx7g -cp ../jsafran/lib/detcrf.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier en.$en.mods -testFile test.$en.tab > test.log
 
-echo "Evaluation"
+echo "Evaluation of the "$en" entity..."
 cut -f2 test.log >! testgold.log
 cut -f3 test.log >! testrec.log
 sed 's,\(.*\)B$,B-\1,g' testgold.log | sed 's,\(.*\)I$,I-\1,g' >! testgold.col
 sed 's,\(.*\)B$,B-\1,g' testrec.log | sed 's,\(.*\)I$,I-\1,g' >! testrec.col
 cut -f1 test.log >! words.col
-paste words.col testgold.col testrec.col | ./conlleval.pl -d '\t' -o NO
+paste words.col testgold.col testrec.col | ./conlleval.pl -d '\t' -o NO | grep $en >> res.log
+
+paste words.col testgold.col testrec.col | ./conlleval.pl -d '\t' -o NO >> res-all.log
 
 end
 
