df3d3d155b22e36c16931419db4153b438c0aa7e
xtoff <cerisara@loria.fr>
2014-11-14 10:52:56 +0100

    test successful

--- src/test/AutoTests.java
+++ src/test/AutoTests.java
@@ -2,6 +2,7 @@ package test;
 
 public class AutoTests {
 	public static void main(String args[]) throws Exception {
-		throw new Exception("Just an example of test that fails");
+//		throw new Exception("Just an example of test that fails");
+		System.out.println("example of test that succeeds");
 	}
 }
